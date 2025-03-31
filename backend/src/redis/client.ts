d369a5f591ab33c550f9af064bd4ac43f7bc8491
nielassis <nielvitorba@gmail.com>
2025-03-30 22:56:50 -0300

    fix: fix(redis): resolve connection issues with Upstash Redis

--- backend/src/redis/client.ts
+++ backend/src/redis/client.ts
@@ -1,4 +1,12 @@
 import Redis from "ioredis";
 import { env } from "../env";
 
-export const redis = new Redis(env.REDIS_URL);
+export const redis = new Redis(env.REDIS_URL, {
+  retryStrategy: (times) => {
+    if (times >= 20) {
+      return null;
+    }
+    return Math.min(times * 50, 2000);
+  },
+  maxRetriesPerRequest: 30,
+});
